3126835f4808ec17495930d6659a928e0e284f42
Maxime <38160587+Moxiii@users.noreply.github.com>
2022-12-03 23:49:19 +0100

    separation du front dir

--- scrappator/back/setup/settings.py
+++ scrappator/back/setup/settings.py
@@ -116,11 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__name__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'front/static')
 STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+print("le base directory est " + BASE_DIR)
+print("le static root est  " + STATIC_ROOT)
